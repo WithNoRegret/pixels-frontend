74ade80e756684d0eb2e976e7d4e85038ed04856
Elvira Latypova <ellalatypova666@gmail.com>
2025-07-23 05:15:37 +0500

    style: update logo

--- src/shared/ui/logo.tsx
+++ src/shared/ui/logo.tsx
@@ -1,3 +1,4 @@
+import { Swords } from "lucide-react";
 import Link from "next/link";
 
 import { cn } from "@/shared/lib";
@@ -8,8 +9,9 @@ interface LogoProps {
 
 export const Logo = ({ className }: LogoProps) => {
   return (
-    <Link href="/" className={cn("", className)}>
+    <Link href="/" className={cn("flex items-center gap-1", className)}>
       <h1 className="text-5xl">Pixel Battle</h1>
+      <Swords size={28} />
     </Link>
   );
 };
